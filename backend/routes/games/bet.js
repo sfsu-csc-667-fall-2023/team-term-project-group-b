5197d15006759eecac3d543ea6ddd850da8467cd
heliumk <erickunzel@gmail.com>
2023-12-08 14:51:27 -0800

    small changes

--- backend/routes/games/bet.js
+++ backend/routes/games/bet.js
@@ -4,6 +4,11 @@ const GAME_CONSTANTS = require("../../../constants/games");
 const method = "post";
 const route = "/:id/bet";
 
+//player enters in amt to bet, mustr match or exceed current bet
+//after bet, checks current seat(current player), and cycles
+//checks if we are at the end of turn 3
+  //at end of turn three, run hand evaluator middleware
+
 const handler = async (request, response) => {
 const io = request.app.get("io");
 
