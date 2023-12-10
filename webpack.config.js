b31fb047845347fc6858c14eb7c0aef9a89c066b
Collins Gichohi <cgichohi2018@gmail.com>
2023-12-10 15:08:10 -0800

    Revert "fixed some webpack errors"
    This reverts commit 9e47006b3f250030b132595c5733f51325b5a51b.

--- webpack.config.js
+++ webpack.config.js
@@ -23,20 +23,6 @@ module.exports = {
   resolve: {
     alias: {
       "@constants": path.resolve(__dirname, "constants"),
-      'node:path': path.resolve(__dirname, 'node_modules/path'),
     },
-    fallback: {
-      url: require.resolve('url/'),
-      fs: false,
-      buffer: require.resolve('buffer/'),
-      stream: require.resolve('stream-browserify'),
-      zlib: require.resolve('browserify-zlib'),
-      crypto: require.resolve('crypto-browserify'),
-      http: require.resolve('stream-http'),
-      querystring: require.resolve('querystring-es3'),
-      assert: require.resolve('assert/'),
-      net: false,
-      async_hooks: require.resolve('async_hooks')
-    }
   },
 };
