f3c8e873b2f8e74cb444c5d4a9bb14fd9a84197d
heliumk <erickunzel@gmail.com>
2023-12-14 16:19:28 -0800

    added checkTwo
    check two checks for first pair

--- backend/utils/checkers/checkTwo.js
+++ backend/utils/checkers/checkTwo.js
@@ -1,5 +1,15 @@
-const checkTwo = () => {
+const checkTwo = (sorted) => {
+    console.log(sorted);
     
+    for (let i = 0; i < sorted.length-1; i++) { 
+        if (sorted[i].value == sorted[i+1].value) {
+            console.log("Found Pair!!!!!!");
+        }
+      }
+
+    return{
+
+    }
 }
 
 module.exports = { checkTwo };
